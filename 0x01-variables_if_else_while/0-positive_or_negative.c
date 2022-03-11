94f0e6e58c11407380f18347380a004f8ba2a00d
Profay <oyewaletimmy01@gmail.com>
2022-03-11 02:00:30 -0800

    works on printing alphabet and printing last digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,29 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-/*main (printing numbers)*/
+#include <time.h>
+#include <stdio.h>
 
-/*int number
- * printf
- * return (results)*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
-		printf("%i is positive\n", n);
+	{
+		printf("%d is positive\n", n);
+	}
 	else if (n < 0)
-		printf("%i is negative\n", n);
+	{
+		printf("%d is negative\n", n);
+	}
 	else
-		printf("%i is zero\n", n);
-
+	{
+		printf("%d is zero\n", n);
+	}
 	return (0);
 }
