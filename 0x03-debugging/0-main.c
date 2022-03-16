55037bcd22744a2486b22091a88161529a5a6046
Profay <oyewaletimmy01@gmail.com>
2022-03-16 15:44:05 -0700

    updating task 0 again

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints
+ * Return: 0
+ */
+
+int nain(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+void positivie_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month,int day, int year);
+
+#endif /* MAIN.H */#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+void positivie_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month,int day, int year);
+
+#endif /* MAIN.H */#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+
+void positivie_or_negative(int i);
+int largest_number(int, int, int);
+int convert_day(int month, int day);
+void print_remaining_days(int month,int day, int year);
+
+#endif /* MAIN.H */
