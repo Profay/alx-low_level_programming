0926ef71a1fc47d049488ae4ae3f74a915ba408b
Profay <oyewaletimmy01@gmail.com>
2022-03-19 00:56:01 -0700

    okay

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,11 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-void positivie_or_negative(int i);
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
-void print_remaining_days(int month,int day, int year);
+void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN.H */
+#endif /* MAIN_H */
