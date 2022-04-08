aac75ffed56b9ed1635faf98b9b4764ce7315517
PROFAY <85462029+Profay@users.noreply.github.com>
2022-04-08 01:24:55 +0100

    Update main.h
    This is where I stopped

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,17 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
+#include <stdlib.h>
+#include <stdio.h>
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+int _atoi(const char *s);
+void print_int(unsigned long int n);
+void _puts(char *str);
 
 #endif
