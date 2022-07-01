d208a4c4abfa52c7c9aceca335514592ef1bbe67
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-01 09:35:57 +0000

    creating hash table

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,23 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stdio.h>
+
+
+typedef struct hash_node_s
+{
+     char *key;
+     char *value;
+     struct hash_node_s *next;
+} hash_node_t;
+
+
+typedef struct hash_table_s
+{
+     unsigned long int size;
+     hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif
