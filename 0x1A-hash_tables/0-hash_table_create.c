0d4e67df7d1dc6c4f556598ad955bb7da2f387a6
PROFAY <Oyewaletimmy01@gmail.com>
2022-07-01 09:47:08 +0000

    push

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,14 +4,21 @@
  * @size: size of the array
  * Return: hash_table_t
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t* table = (hash_table_t*) malloc (sizeof(hash_table_t));
-    table->size = size;
-    table->array= (hash_node_t**) calloc (table->size, sizeof(hash_node_t*));
-    for (int i=0; i<table->size; i++)
-        table->array[i] = NULL;
- 
-    return table;
+	hash_table_t *new;
+
+	if (size == 0)
+		return (NULL);
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+	new->size = size;
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	return (new);
 }
