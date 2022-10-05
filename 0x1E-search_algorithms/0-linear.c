f26ae2246f96d576cb4b3e6a8b2105ba14470d1c
PROFAY <Oyewaletimmy01@gmail.com>
2022-10-05 13:46:27 +0000

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,18 +9,15 @@
  * @value: value to search in
  * Return: Always EXIT_SUCCESS
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-size_t index = 0;
+	size_t i;
 
-for (index = 0; index < size; index++)
-{
-printf("Value checked array[%ld] = [%d]\n", index, array[index]);
-if (array[index] == value)
-{
-return (index);
-}
-}
-return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
